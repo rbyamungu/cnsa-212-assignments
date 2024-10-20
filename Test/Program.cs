7cebcc1be38885ae5de63fc848c693caa2e0a64d
Rene Ruhigita <rbyamungu37@gmail.com>
2024-10-20 11:24:07 -0400

    Nothing change i was just testing somethings

--- Test/Program.cs
+++ Test/Program.cs
@@ -6,7 +6,7 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-	    string User = args.Length > 0 ? args[0] : "user";
+	  string User = args.Length > 0 ? args[0] : "user";
 
             Console.WriteLine($"Hi from CNSA 212, {User}!");
         }
