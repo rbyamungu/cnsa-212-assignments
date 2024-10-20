1efa19884cd72e5d9a839a84b15d82cd07599cee
Rene Ruhigita <rbyamungu37@gmail.com>
2024-10-20 00:44:30 -0400

    Add the If statement

--- Echo/Program.cs
+++ Echo/Program.cs
@@ -6,7 +6,15 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is echo");
+            string userInput = args.Length[0] > 0 ? args[0] : "userInput";
+            if (userInput[0] == "R"){
+
+                Console.WriteLine($"{reverse(userInput)}");
+            }
+            else {
+                Console.WriteLine($"{userInput}")
+            }
+            
         }
     }
 }
