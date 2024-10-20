e610a94fd73bc241d422ead8aeccb5b3c5676d64
Rene Ruhigita <rbyamungu37@gmail.com>
2024-10-19 22:57:29 -0400

    add new folders and Edit the HelloWorld program file

---
+++ Test/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+	    string User = args.Length > 0 ? args[0] : "user";
+
+            Console.WriteLine($"Hi from CNSA 212, {User}!");
+        }
+    }
+}
