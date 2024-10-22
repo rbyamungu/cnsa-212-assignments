321e94c36134205df6041dd0324487a72c03d885
Rene Ruhigita <rbyamungu37@gmail.com>
2024-10-22 13:39:46 -0400

    New assignment

---
+++ TerribleCalculator/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0){
+                for (int i = 0; i < args.Length; i++){
+                    Console.WriteLine($"Hello, {args[i]}!");
+                    Console.ReadLine($"Goodbye, {args[i]}!");
+                }
+            }
+        }
+    }
+}
