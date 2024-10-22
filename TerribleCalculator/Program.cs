39bfc519f7e2eb63c85185bb701adfe4c109bc4b
Rene Ruhigita <rbyamungu37@gmail.com>
2024-10-22 16:29:01 -0400

    Add New Program

--- TerribleCalculator/Program.cs
+++ TerribleCalculator/Program.cs
@@ -1,18 +1,39 @@
 ﻿
 using System;
 
-namespace MyApp
+
+namespace calculate
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            if (args.Length > 0){
-                for (int i = 0; i < args.Length; i++){
-                    Console.WriteLine($"Hello, {args[i]}!");
-                    Console.ReadLine($"Goodbye, {args[i]}!");
+            double timesSt = 0;
+            string input ;
+
+            Console.WriteLine("Let's calculate badly: I hope you like adding because that's all we support. Enter the values to calculate on one line at a time. When you are done, enter the text end or press CTRL+Z on Windows or CTRL+D on linux.");
+            
+            while(true)
+            {
+                input = Console.ReadLine();
+                
+
+                if (input == "end" || input == null /*|| Console.ReadKey().Key == (ConsoleKey.CTRL | C)*/ )
+                {
+                    Console.WriteLine($"Total:" + timesSt);
+                    break;
                 }
+                
+                if (int.TryParse(input, out int num)){
+                    timesSt +=  num;
+                }
+                else{
+                    Console.WriteLine("I'm sorry, I couldn't understand that. Please enter a valid number.");
+                }
+
+                
             }
+            
         }
     }
 }
