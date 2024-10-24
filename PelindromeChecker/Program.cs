761290d0ebda79e957d6555158f7498ebe3c7b91
Rene Ruhigita <rbyamungu37@gmail.com>
2024-10-24 02:08:44 -0400

    The Pelindom

---
+++ PelindromeChecker/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program 
+    {
+        static void Main(string[] userInput)
+        {
+            Console.WriteLine("Greetings Professor Falken. Do you want to play a game? Let's find palindromes. Enter a potential palindrome and press enter");
+            
+            string Test = Console.ReadLine();
+            string NoSpace = Test.Trim();
+
+            if ( NoSpace == Array.Reverse(NoSpace)){
+                Console.WriteLine("Palindrome unlocked");
+            }
+            else{
+                Console.WriteLine("Whomp, whomp.");
+            }
+            
+        }
+    }
+}
