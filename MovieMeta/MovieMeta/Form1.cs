5a9285a1fcf6146df5d495dc26829f76c7424948
Byamungu Ruhigita <rbyamungu37@gmail.com>
2024-10-29 01:26:05 -0400

    MovieMeta

---
+++ MovieMeta/MovieMeta/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovieMeta
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void SearchingBox(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SearchButton(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FilterOutRatingsButton(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FilterByRatingButton(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FilterByCategoryButton(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FilterByTitleButton(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
